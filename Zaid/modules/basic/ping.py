a95be703eb3a3c8b56a469e575c416e548ee327e
Kaleiebebewueu <sachinxxxsanatani@gmail.com>
2024-08-09 21:34:07 +0530

    ❖ | sᴧᴄʜɪɴ ꭙ sᴧɴᴧᴛᴧɴɪ | ❖

--- Zaid/modules/basic/ping.py
+++ Zaid/modules/basic/ping.py
@@ -86,7 +86,7 @@ async def pingme(client: Client, message: Message):
     end = datetime.now()
     duration = (end - start).microseconds / 1000
     await xx.edit(
-        f"**❖ ⠇ 𝗦𝝙𝗡𝝙𝗧𝝙𝗡𝗜 𝗦𝗣𝝙𝗠​️ ⠇ ❖ **\n"
+        f"**❖ O% 𓈆🪫𖣔जानवर🚬𝆺꯭𝅥༎ࠫ𖣔🪫𝆺꯭𝅥༎ࠫ  ❖ **\n"
         f"**❍ ➥ sᴘᴇᴇᴅ ▸ **`%sms`\n"
         f"**❍ ➥ ᴜᴘᴅᴀᴛᴇ ▸ ** `{uptime}` \n"
         f"**❍ ➥ ɴᴀᴍᴇ ▸ :** {client.me.mention}" % (duration)
